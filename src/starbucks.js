bd864f7e8d3feeac3e76864ea36c995f289a5e0d
Fernando de Araujo Lopes <nando.alopes84@gmail.com>
2026-05-13 21:52:11 -0300

    Criaçnao da nova função contabilizarQuantidadeDeItem com teste unitário passando aprovado

--- src/starbucks.js
+++ src/starbucks.js
@@ -1,14 +1,29 @@
 export function contabilizarQuantidadeDeCafe(listaPedidos) {
 
-    let quantidadeCafe = 0
+    let quantidadeDeCafe = 0
 
     for (let i = 0; i < listaPedidos.length; i++) {
 
         if (listaPedidos[i].nome == "café") {
-            quantidadeCafe++
+            quantidadeDeCafe++
         }
     }
 
-    return quantidadeCafe
+    return quantidadeDeCafe
+
+}
+
+export function contabilizarQuantidadeDeItem(itemEsperado, listaPedidos){
+
+    let quantidadeItemEsperado = 0
+
+    for (let i = 0; i < listaPedidos.length; i++) {
+
+        if (listaPedidos[i].nome == itemEsperado) {
+            quantidadeItemEsperado++
+        }
+    }
+
+    return quantidadeItemEsperado
 
 }
--- test/starbucks.test.js
+++ test/starbucks.test.js
@@ -1,9 +1,9 @@
 import assert from 'node:assert'
-import { contabilizarQuantidadeDeCafe } from '../src/starbucks.js'
+import { contabilizarQuantidadeDeCafe, contabilizarQuantidadeDeItem } from '../src/starbucks.js'
 
 describe('Testes para gestão de cafeteria', () => {
 
-    it('TC 1 - Ao menos 1 café na lista', () => {
+    it('TC 1 - Ao menos 1 item na lista', () => {
         // Arrange
         const listaPedidos = [
             { nome: "café", valor: 4.00 },
@@ -20,7 +20,7 @@ describe('Testes para gestão de cafeteria', () => {
 
     });
 
-    it('TC 2 - Não ter nenhum café na lista', () => {
+    it('TC 2 - Não ter nenhum item na lista', () => {
         // Arrange
         const listaPedidos = [
             { nome: "bolo de cenoura", valor: 12.00 },
@@ -36,7 +36,7 @@ describe('Testes para gestão de cafeteria', () => {
 
     });
 
-    it('TC 3 - Ter 2 cafés na lista', () => {
+    it('TC 3 - Ter 2 itens na lista', () => {
         // Arrange
         const listaPedidos = [
             { nome: "café", valor: 4.00 },
@@ -55,7 +55,7 @@ describe('Testes para gestão de cafeteria', () => {
 
     });
 
-        it('TC 4 - Retornar uma lista de pedidos vazia', () => {
+    it('TC 4 - Retornar uma lista de pedidos vazia', () => {
         // Arrange
         const listaPedidos = []
         const retornoEsperado = 0
@@ -65,6 +65,26 @@ describe('Testes para gestão de cafeteria', () => {
 
         // Assert
         assert.equal(quantidadeDeCafes, retornoEsperado)
+
+    });
+
+    it('TC 5 - Filtrar por um item específico da lista', () => {
+        // Arrange
+        const listaPedidos = [
+            { nome: "café", valor: 4.00 },
+            { nome: "bolo de cenoura", valor: 12.00 },
+            { nome: "café com leite", valor: 5.00 },
+            { nome: "café", valor: 4.00 },
+            { nome: "café com leite", valor: 5.00 },
+        ]
+        const itemPesquisado = "bolo de cenoura"
+        const retornoEsperado = 1
+
+        // Act
+        const quantidadeDeItens = contabilizarQuantidadeDeItem(itemPesquisado, listaPedidos)
+
+        // Assert
+        assert.equal(quantidadeDeItens, retornoEsperado)
 
     });
 
